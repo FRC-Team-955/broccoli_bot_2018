b90341ccf80707537c8d7eaab016ca18e08edc79
Masterchef365 <duncan.freeman1@gmail.com>
2018-04-17 19:14:03 -0700

    Crappy morph

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <raw_video.hpp>
 #include <vector>
+#include <chrono>
 
 const char *trackbar_save_name = "config.yml";
 const char *trackbar_win_name = "config";
@@ -15,10 +16,9 @@ using namespace cv;
 int main(int argc, char **argv) {
 	if (argc > 1) {
 
+		typedef std::chrono::high_resolution_clock Clock;
 		Point3i hsv_min(0, 0, 0);
 		Point3i hsv_max(255, 255, 255);
-		int blur_size = 0;
-		int is_broc_thresh = 0;
 
 		namedWindow(trackbar_win_name);
 
@@ -29,8 +29,6 @@ int main(int argc, char **argv) {
 #define READ_ENT(NAME) fs[#NAME] >> NAME;
 				READ_ENT(hsv_min);
 				READ_ENT(hsv_max);
-				READ_ENT(blur_size);
-				READ_ENT(is_broc_thresh);
 #undef READ_ENT
 			} else {
 				std::cerr << "Error reading " << trackbar_save_name << std::endl;
@@ -45,8 +43,6 @@ int main(int argc, char **argv) {
 		createTrackbar("hsv_max_sat", trackbar_win_name, &hsv_max.y, 255);
 		createTrackbar("hsv_max_val", trackbar_win_name, &hsv_max.z, 255);
 
-		createTrackbar("is_broc_thresh", trackbar_win_name, &is_broc_thresh, 255);
-		createTrackbar("blur_size", trackbar_win_name, &blur_size, 255);
 
 		Size frame_size;
 
@@ -65,11 +61,9 @@ int main(int argc, char **argv) {
 		Mat hsv;
 		Mat hsv_mask;
 		Mat laplacian_out;
-		Mat lap_blur;
 		Mat correct_color_lap;
-		Mat is_broc;
-		Mat is_broc_canny;
-		Mat color_visual;
+		Mat disp;
+		Mat morph_kern = getStructuringElement(MORPH_ELLIPSE, Size(15, 15));
 
 		std::vector<std::vector<Point>> contours;
 		std::vector<Point> convex;
@@ -80,7 +74,6 @@ int main(int argc, char **argv) {
 			if (wrap) {
 				wrap->wait_for_frames(depth_frame, color_frame);
 			}
-			color_frame.copyTo(color_visual);
 			cvtColor(color_frame, hsv, CV_BGR2HSV);
 
 			inRange(hsv, Scalar(hsv_min.x, hsv_min.y, hsv_min.z),
@@ -92,36 +85,13 @@ int main(int argc, char **argv) {
 
 			laplacian_out.copyTo(correct_color_lap, hsv_mask);
 
-			blur(correct_color_lap, lap_blur,
-					cv::Size(1 + blur_size * 2, 1 + blur_size * 2));
+			inRange(correct_color_lap, Scalar(1, 1, 1), Scalar(255, 255, 255), disp);
+			dilate(disp, disp, morph_kern);
 
-			inRange(lap_blur,
-					Scalar(is_broc_thresh, is_broc_thresh, is_broc_thresh),
-					Scalar(255, 255, 255), is_broc);
-
-			Canny(is_broc, is_broc_canny, 128, 128);
-			//Rect bound (1, 1, is_broc_canny.rows - 1, is_broc_canny.cols - 1);
-			//rectangle(is_broc_canny, bound, Scalar(0,0,0));
-
-			findContours(is_broc_canny, contours, CV_RETR_EXTERNAL,
-					CV_CHAIN_APPROX_SIMPLE);
-
-			for (int i = 0; i < contours.size(); i++) {
-				convexHull(contours[i], convex, true);
-				//polylines(color_visual, convex, true, Scalar(255, 0, 255), 2);
-				fillConvexPoly(color_visual, convex, Scalar(0, 128, 255));
-				//drawContours(color_visual, convex, 0, Scalar(0, 255, 0));
-			}
-
-			// imshow("Depth", depth_frame * 10);
-			// imshow("Color", color_frame);
-			// imshow("hsv", hsv_mask);
-			imshow("out", color_visual);
-			imshow("cann", is_broc_canny);
-			// imshow("lap", correct_color_lap * 5);
-			// imshow("lap_b", lap_blur * 15);
+			imshow("disp", disp);
 			if (!pause && !raw.next(depth_frame, color_frame)) {
 				raw.reset_index();
+				run_app = false;
 			}
 			switch (waitKey(30)) {
 				case 'q':
@@ -139,8 +109,6 @@ int main(int argc, char **argv) {
 #define WRITE_ENT(NAME) fs << #NAME << NAME;
 			WRITE_ENT(hsv_min);
 			WRITE_ENT(hsv_max);
-			WRITE_ENT(is_broc_thresh);
-			WRITE_ENT(blur_size);
 #undef WRITE_ENT
 		} else {
 			std::cerr << "Error writing " << trackbar_save_name << std::endl;
