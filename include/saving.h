cc27ec8d72eb707bbe8dc65accb47eb6ef99ac87
Masterchef365 <duncan.freeman1@gmail.com>
2018-05-29 13:18:51 -0700

    Kinda works, needs segmentation

---
+++ include/saving.h
@@ -0,0 +1,13 @@
+PARAM(hue_min, true)
+PARAM(hue_max, true)
+
+PARAM(sat_min, true)
+PARAM(sat_max, true)
+
+PARAM(val_min, true)
+PARAM(val_max, true)
+
+PARAM(lap_thresh, true)
+PARAM(morph_size, false)
+
+PARAM(roi_squeeze, true)
--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,24 @@
 #include <chrono>
 #include <stdio.h>
 
-const char *trackbar_save_name = "config.yml";
-const char *trackbar_win_name = "config";
+#define DISP(MAT) imshow(#MAT, MAT);
+
+const char* trackbar_save_name = "config.yml"; //TODO: turn this into an argument
+const char* trackbar_win_name = "config";
 using namespace cv;
 
 int main(int argc, char **argv) {
 	if (argc > 1) {
-
-#define PARAM(NAME) int NAME;
+		bool is_live = argc > 2;
+#define PARAM(NAME, TRACKBAR) int NAME;
 #include <saving.h>
 #undef PARAM
-
 		namedWindow(trackbar_win_name);
 
 		{
 			FileStorage fs;
 			if (fs.open(trackbar_save_name, cv::FileStorage::FORMAT_YAML | cv::FileStorage::READ)) {
-#define PARAM(NAME) fs[#NAME] >> NAME;
+#define PARAM(NAME, TRACKBAR) fs[#NAME] >> NAME;
 #include <saving.h>
 #undef PARAM
 			} else {
@@ -37,109 +38,79 @@ int main(int argc, char **argv) {
 			}
 		}
 
-#define PARAM(NAME) createTrackbar(#NAME, trackbar_win_name, &NAME, 255);
+#define PARAM(NAME, TRACKBAR) if (TRACKBAR) createTrackbar(#NAME, trackbar_win_name, &NAME, 255);
 #include <saving.h>
 #undef PARAM
 
-		Size frame_size;
+		Mat morph_element = getStructuringElement (0, Size (2 * morph_size + 1, 2 * morph_size + 1));
 
+		Size frame_size;
+		
+		frame_size = Size(640, 480);
 		CVRealsenseWrapper *wrap = 0;
-		if (argc > 2) {
-			frame_size = Size(640, 480);
-			wrap = new CVRealsenseWrapper(Size(640, 480), frame_size, 30);
+		if (is_live) {
+			wrap = new CVRealsenseWrapper(frame_size, frame_size, 30);
 		}
 
-		RawVideo raw(argv[1], argc > 2 ? RawVideo::Mode::Write : RawVideo::Mode::Read, frame_size);
+		RawVideo raw(argv[1], is_live ? RawVideo::Mode::Write : RawVideo::Mode::Read, frame_size);
 
-		Mat color_frame_sz = Mat::zeros(frame_size, CV_8UC3);
-		Mat depth_frame_sz = Mat::zeros(frame_size, CV_16UC1);
-		Mat color_frame;
-		Mat depth_frame;
-		Mat hsv;
-		Mat hsv_mask;
-		Mat laplacian_out;
-		Mat correct_color_lap;
-		Mat morph_out;
-		Mat colors[3];
-		Mat morph_kern = getStructuringElement(MORPH_ELLIPSE, Size(7, 7));
+		Mat depth_frame, color_frame;
+		Mat depth_frame_roi, color_frame_roi;
+		Mat color_hsv, hsv_mask;
+		Mat color_split[3];
+		Mat laplacian, laplacian_confirmed;
+		Mat lap_conf_mask_combined;
 
-		std::vector<std::vector<Point>> contours;
-
-		float downsample = 2.0;
+		Mat morph_blob;
 
 		bool run_app = true;
 		bool pause = false;
 		do {
 			if (wrap) {
-				wrap->wait_for_frames(depth_frame_sz, color_frame_sz);
+				wrap->wait_for_frames(depth_frame, color_frame);
 			}
-			resize(color_frame_sz, color_frame, Size(), 1.0 / downsample, 1.0 / downsample);
-			split(color_frame, colors);
-			/*
-			resize(depth_frame_sz, depth_frame, Size(), 0.25, 0.25);
-			cvtColor(color_frame, hsv, CV_BGR2HSV);
-			correct_color_lap.setTo(0);
-			inRange(hsv, Scalar(hue_min, sat_min, val_min), Scalar(hue_max, sat_max, val_max), hsv_mask);
-			laplacian_out.copyTo(correct_color_lap, hsv_mask);
-			*/
 
-			Laplacian(colors[1], laplacian_out, CV_8UC1, 1);
-			inRange(laplacian_out, Scalar(is_broc_thresh), 255, laplacian_out);
-			imshow("lap", laplacian_out);
-			std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-			//Mat nacced = nac(laplacian_out, smooth + 1, (float)thresh / 16.0);
-
-			dilate(laplacian_out, morph_out, morph_kern);
-			erode(morph_out, morph_out, morph_kern);
-			std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-    		auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-			std::cout << duration << std::endl;
-			
-			/*
-			findContours(nacced, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-			Mat final;	
-			color_frame.copyTo(final);
-			for (int i = 0; i < contours.size(); i++) {
-				std::vector<Point> hull;
-				convexHull(contours[i], hull);
-				if (contourArea(hull) > area_thresh * area_thresh * area_thresh) {
-					polylines(final, hull, true, Scalar(255, 0, 204), 2);
-					Point accum;
-					for (auto& pt : hull)
-						accum += pt;
-					accum.x /= hull.size();
-					accum.y /= hull.size();
-					circle(final, accum, 4, Scalar(255, 255, 0));
-				}
-			}
-			*/
-
-			imshow("morph_out", morph_out);
-			//imshow("hsv", hsv_mask);
-			//imshow("color", (colors[1] * 3) - colors[0] - colors[2]);
-			//imshow("nac", nacced);
-			//imshow("color", final);
-			if (!pause && !raw.next(color_frame_sz, depth_frame_sz)) {
+			if (!pause && !raw.next(color_frame, depth_frame)) {
 				raw.reset_index();
-				run_app = false;
+				return 0;
 			}
-			switch (waitKey(30)) {
+			switch (waitKey(1)) {
 				case 'q':
 					run_app = false;
 					break;
 				case ' ':
 					pause = !pause;
 					break;
-				case 'a':
-					imwrite("/home/duncan/cap.png", laplacian_out);
-					break;
 			}
+			
+			int roi_squeeze_px = (float(roi_squeeze) / 255.0) * (frame_size.height / 2);
+			Rect roi (roi_squeeze_px, 0, frame_size.height - (2 * roi_squeeze_px), frame_size.width);
+			//std::cout << roi_squeeze_px << ", " << roi << ", " << frame_size.width << std::endl;
+			//rectangle(color_frame, roi, Scalar(255, 0, 255), 2);
+			color_frame_roi = color_frame(roi);
+			depth_frame_roi = depth_frame(roi);
+
+			imshow("color", color_frame_roi);
+			imshow("depth", depth_frame_roi * 3);
+			cvtColor(color_frame_roi, color_hsv, CV_RGB2HSV);
+			inRange(color_hsv, Scalar(hue_min, sat_min, val_min), Scalar(hue_max, sat_max, val_max), hsv_mask);
+			//DISP(hsv_mask);
+			split(color_frame_roi, color_split);
+			Laplacian(color_split[1], laplacian, CV_8UC1);
+			inRange(laplacian, Scalar(lap_thresh), Scalar(255), laplacian_confirmed);
+			//DISP(laplacian_confirmed);
+			bitwise_and(laplacian_confirmed, hsv_mask, lap_conf_mask_combined);
+			DISP(lap_conf_mask_combined);
+			morphologyEx(lap_conf_mask_combined, morph_blob, MORPH_CLOSE, morph_element);
+			morphologyEx(morph_blob, morph_blob, MORPH_OPEN, morph_element);
+			DISP(morph_blob);
+
 		} while (run_app);
 
 		FileStorage fs;
 		if (fs.open(trackbar_save_name,
 					cv::FileStorage::FORMAT_YAML | cv::FileStorage::WRITE)) {
-#define PARAM(NAME) fs << #NAME << NAME;
+#define PARAM(NAME, TRACKBAR) fs << #NAME << NAME;
 #include <saving.h>
 #undef PARAM
 		} else {
@@ -151,3 +122,4 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 }
+#undef MAT
